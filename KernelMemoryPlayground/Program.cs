bad580e0d000b3e56fee49e3ca9484766089904b
Lukasz Jozwik <ext.lukasz.jozwik@softwareone.com>
2024-04-03 19:05:39 +0200

    Add project files.

---
+++ KernelMemoryPlayground/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.KernelMemory;
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var openAiApiKey = configuration["OPENAI_API_KEY"] ?? throw new InvalidOperationException("OPENAI_API_KEY");
+
+var memory = new KernelMemoryBuilder()
+    .WithOpenAIDefaults(openAiApiKey)
+    .WithQdrantMemoryDb("http://localhost:6333/")
+    .Build<MemoryServerless>();
+
+var importDocuments = false;
+if (importDocuments)
+{
+    await memory.ImportDocumentAsync("Resources/C01L01.md", tags: new TagCollection
+    {
+        { "lesson", "C01L01" },
+        { "title", "Wprowadzenie do Generative AI"}
+    });
+    await memory.ImportDocumentAsync("Resources/C01L02.md", tags: new TagCollection
+    {
+        { "lesson", "C01L02" },
+        { "title", "Zasady działania LLM"}
+    });
+    await memory.ImportDocumentAsync("Resources/C01L03.md", tags: new TagCollection
+    {
+        { "lesson", "C01L03" },
+        { "title", "Prompt Design"}
+    });
+    await memory.ImportDocumentAsync("Resources/C01L04.md", tags: new TagCollection
+    {
+        { "lesson", "C01L04" },
+        { "title", "OpenAI API"}
+    });
+    await memory.ImportDocumentAsync("Resources/C01L05.md", tags: new TagCollection
+    {
+        { "lesson", "C01L05" },
+        { "title", "Prompt Engineering"}
+    });
+}
+
+while (true)
+{
+    Console.Write("Question:");
+    var question = Console.ReadLine();
+    if (string.IsNullOrEmpty(question)) break;
+
+    var answer = await memory.AskAsync(question);
+    Console.WriteLine($"AI: {answer.Result}");
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # KernelMemoryPlayground
+
+Playground console app testing KernelMemory package with generating knowledge database using markdown files from aidevs.pl course.
+
+## How to run
+
+1. Run https://qdrant.tech/ with default ports
+1. Add `OPENAI_API_KEY` to user secrets of KernelMemoryPlayground.csproj
+1. Upload markdown files to folder KernelMemoryPlayground/Resources/ - [you can get them from here](https://bravecourses.circle.so/c/informacje-ai2r/tresci-lekcji-w-formacie-tekstowym-markdown-do-pobrania)
+1. Change flag importDocuments to true
+1. Run the app using dotnet
